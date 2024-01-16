0252d602c014fd7e97c6ad078acde5f5fef5d6a4
Jahdai <jahdai.kilkenny@gmail.com>
2024-01-16 10:53:34 -0500

    scaffolding created, book and review schemas created

---
+++ models/book.js
@@ -0,0 +1,24 @@
+//This file creates the Book model/schema
+
+//Require the Mongoos package
+const mongoose = require("mongoose");
+const reviewSchema = require('./review.js')
+
+//Create a schema to define the properties of the book collection:
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  photo: { type: String, required: true },
+  synopsis: { type: String, required: true },
+  isBookOfMonth: { type: Boolean, default: false },
+  isFeatured: { type: Boolean, default: false },
+  bomMonth: { type: String },
+  bomYear: { type: String, minlength: 4, maxlength: 4 },
+  //Only accepting values from reviews schema
+  reviews: [reviewSchema]
+});
+
+//Export schema as a mongoose model
+module.exports=mongoose.model('Book', bookSchema)
---
+++ models/review.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+
+  reviewTitle: {
+    type: String,
+  },
+
+  review: {
+    type: String,
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  dateAdded: { type: Date, default: Date.now },
+});
+
+module.exports = reviewSchema;
